142ac7f8cf3b723a828f68eef51db8fc33a2d1d8
Lonely Wolf <119653343+LonelyWolfYT@users.noreply.github.com>
2026-08-03 21:50:31 +0530

    port fix

--- app.py
+++ app.py
@@ -151,4 +151,5 @@ async def process_car_image(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=5000, reload=True)
+    port = int(os.getenv("PORT", 5000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
